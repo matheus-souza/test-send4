f987fb40f79e901191c54c6c6d01f6c3b5a07554
Matheus H. de Souza <mh.matheussouza@gmail.com>
2019-07-31 23:30:16 -0300

    Adicionado views crud do contato

---
+++ resources/views/contacts/show.blade.php
@@ -0,0 +1,40 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <div class="row">
+            @include('admin.sidebar')
+
+            <div class="col-md-9">
+                <div class="card">
+                    <div class="card-header">contact {{ $contact->id }}</div>
+                    <div class="card-body">
+
+                        <a href="{{ url('/contacts') }}" title="Back"><button class="btn btn-warning btn-sm"><i class="fa fa-arrow-left" aria-hidden="true"></i> Back</button></a>
+                        <a href="{{ url('/contacts/' . $contact->id . '/edit') }}" title="Edit contact"><button class="btn btn-primary btn-sm"><i class="fa fa-pencil-square-o" aria-hidden="true"></i> Edit</button></a>
+
+                        <form method="POST" action="{{ url('contacts' . '/' . $contact->id) }}" accept-charset="UTF-8" style="display:inline">
+                            {{ method_field('DELETE') }}
+                            {{ csrf_field() }}
+                            <button type="submit" class="btn btn-danger btn-sm" title="Delete contact" onclick="return confirm(&quot;Confirm delete?&quot;)"><i class="fa fa-trash-o" aria-hidden="true"></i> Delete</button>
+                        </form>
+                        <br/>
+                        <br/>
+
+                        <div class="table-responsive">
+                            <table class="table">
+                                <tbody>
+                                    <tr>
+                                        <th>ID</th><td>{{ $contact->id }}</td>
+                                    </tr>
+                                    <tr><th> Nome </th><td> {{ $contact->nome }} </td></tr><tr><th> Sobrenome </th><td> {{ $contact->sobrenome }} </td></tr><tr><th> Email </th><td> {{ $contact->email }} </td></tr>
+                                </tbody>
+                            </table>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
